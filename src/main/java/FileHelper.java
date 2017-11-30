23d1171c1b7daa233978681e88f8f189a4322b1d
Vishal <vishal.b1989@gmail.com>
2017-11-30 17:49:41 -0500

    Added file parser class

---
+++ src/main/java/FileHelper.java
@@ -0,0 +1,134 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class FileHelper {
+    private ApplicationProperties prop;
+
+
+    /**
+     * Class that handles File operations
+     * @param properties Instance of ApplicationProperties
+     */
+    FileHelper(ApplicationProperties properties) {
+        this.prop = properties;
+
+    }
+
+    /**
+     * Reads the file which has the subgraph text
+     * @return List<String[]> list that is equivalent of file with each item in list being a line in the file and
+     * each item within the string array being each word in the file delimited by space.
+     */
+    List<String[]> readFile(File file, String delimiter){
+        List<String[]> list;
+        try (Stream<String> stream = Files.lines(Paths.get(file.getAbsolutePath()))) {
+            list = stream
+                    .map(line->line.split(Pattern.quote(delimiter)))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println("File Not Found at the specified Path");
+            return null;
+        }
+        return list;
+    }
+
+
+    /**
+     * Returns List of Files present at given path
+     * @param path
+     * @return
+     */
+    Stream<File> getAllFileNames(String path){
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        return Stream.of(files);
+    }
+
+    /**
+     * Reads a Ground Truth File, searches for specific target and query pair and converts it into a Map
+     * Where key is the search node within the query and its value is a list of nodes it matches within the target file.
+     * @param file Ground Truth File
+     * @param target name of the target file to find within ground truth file.
+     * @param query name of the query file to find within the ground truth file.
+     * @return Solution Map of specific query applied to given target.
+     */
+    Map<String,List<Integer>> readGroundTruth(File file, String target, String query){
+        List<String[]> groundTruthFile = readFile(file,":");
+        boolean targetRead = false;
+        boolean blockMatch = false;
+        boolean solutionMode = false;
+        Map<String,List<Integer>> solutionMap = new HashMap<>();
+        int currentSolutionIndex=0;
+        int totalSolutions=0;
+        if (groundTruthFile != null) {
+            for (String [] line: groundTruthFile) {
+                if(line.length>1){
+                    if(targetRead && line[0].equals("P") && line[1].equals(query)){
+                        blockMatch = true;
+                        continue;
+                    }
+                    if(targetRead && !blockMatch)
+                        targetRead = false;
+                    if(line[0].equals("T") && line[1].equals(target+".grf")){
+                        targetRead = true;
+                        continue;
+                    }
+                    if(targetRead && blockMatch && line[0].equals("N")){
+                        totalSolutions = Integer.parseInt(line[1]);
+                        solutionMode = true;
+                        continue;
+                    }
+                    if(solutionMode){
+                        if(currentSolutionIndex<totalSolutions){
+                            String[] pairs = line[2].split(Pattern.quote(";"));
+                            for (String pair: pairs) {
+                                String[] solution = pair.split(Pattern.quote(","));
+                                String key = "U"+solution[0];
+                                int value = Integer.parseInt(solution[1]);
+                                populateResultMap(solutionMap,key,value);
+                            }
+                            currentSolutionIndex++;
+                        }else {
+                            solutionMode = false;
+                            targetRead=false;
+                            blockMatch=false;
+                        }
+                    }
+                }
+            }
+        }
+        return solutionMap;
+    }
+
+    /**
+     * Get all file names present in the folder and filter them based on number of nodes
+     * @param path folder path
+     * @param filterNodes number of nodes
+     * @return list of files.
+     */
+    Stream<File> getAllFileNames(String path, int filterNodes){
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        Stream<File> fileStream = Stream.of(files);
+        fileStream = fileStream.filter(file -> file.getName().contains("."+filterNodes+"."));
+        return fileStream;
+    }
+
+    void populateResultMap( Map<String,List<Integer>>  solutionMap, String key, int value){
+        if(solutionMap.containsKey(key)){
+            solutionMap.get(key).add(value);
+        }else {
+            List<Integer> list = new ArrayList<>();
+            list.add(value);
+            solutionMap.put(key,list);
+        }
+    }
+}
+
